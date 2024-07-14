158b33d854f5364dfac4f54cb933fd6b400ce8c4
higarrett <60952275+higarrett@users.noreply.github.com>
2024-07-14 17:08:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,33 +14,40 @@ const scores = {
 };
 
 const questions = {
-  'cat1-1000': { question: 'How big is this line and how big can it get? How big is this line and how big can it get? How big is this line and how big can it get? How big is this line and how big can it get? How big is this line and how big can it get? How big is this line and how big can it get? How big is this line and how big can it get? ', answer: 'Answer 1 for Cat 1 - 1000' },
-  'cat2-1000': { question: 'Something small', answer: 'Answer 2 for Cat 2 - 1000' },
-  'cat3-1000': { question: 'Something medium size but nothing completely crazy', answer: 'Answer 3 for Cat 3 - 1000' },
-  'cat4-1000': { question: 'Question 3 for Cat 3 - 1000', answer: 'Answer 3 for Cat 3 - 1000' },
-  'cat5-1000': { question: 'Question 3 for Cat 3 - 1000', answer: 'Answer 3 for Cat 3 - 1000' },
-  'cat1-2000': { question: 'Question 1 for Cat 1 - 2000', answer: 'Answer 1 for Cat 1 - 2000' },
-  'cat2-2000': { question: 'Question 2 for Cat 2 - 2000', answer: 'Answer 2 for Cat 2 - 2000' },
-  'cat3-2000': { question: 'Question 3 for Cat 3 - 2000', answer: 'Answer 3 for Cat 3 - 2000' },
-  'cat4-2000': { question: 'Question 3 for Cat 3 - 2000', answer: 'Answer 3 for Cat 3 - 2000' },
-  'cat5-2000': { question: 'Question 3 for Cat 3 - 2000', answer: 'Answer 3 for Cat 3 - 2000' },
-  'cat1-3000': { question: 'Question 1 for Cat 1 - 3000', answer: 'Answer 1 for Cat 1 - 3000' },
-  'cat2-3000': { question: 'Question 2 for Cat 2 - 3000', answer: 'Answer 2 for Cat 2 - 3000' },
-  'cat3-3000': { question: 'Question 3 for Cat 3 - 3000', answer: 'Answer 3 for Cat 3 - 3000' },
-  'cat4-3000': { question: 'Question 3 for Cat 3 - 3000', answer: 'Answer 3 for Cat 3 - 3000' },
-  'cat5-3000': { question: 'Question 3 for Cat 3 - 3000', answer: 'Answer 3 for Cat 3 - 3000' },
+  // Code Names
+  'cat1-1000': { question: 'Jaguar, Panther, Tiger, Leopard, Snow Leopard, Lion, Mountain Lion', answer: 'Answer: Mac OS' },
+  'cat1-2000': { question: 'Natty Narwhal, Xenial Xerus, Bionic Beaver, Focal Fossa, Jammy Jellyfish', answer: 'Answer: Ubuntu' },
+  'cat1-3000': { question: 'Chicago, Daytona, Memphis, Whistler, Longhorn, Blackcomb, Blue', answer: 'Answer: Windows' },
   'cat1-4000': { question: 'Question 1 for Cat 1 - 4000', answer: 'Answer 1 for Cat 1 - 4000' },
-  'cat2-4000': { question: 'Question 2 for Cat 2 - 4000', answer: 'Answer 2 for Cat 2 - 4000' },
-  'cat3-4000': { question: 'Question 3 for Cat 3 - 4000', answer: 'Answer 3 for Cat 3 - 4000' },
-  'cat4-4000': { question: 'Question 3 for Cat 3 - 4000', answer: 'Answer 3 for Cat 3 - 4000' },
-  'cat5-4000': { question: 'Question 3 for Cat 3 - 4000', answer: 'Answer 3 for Cat 3 - 4000' },
-  'cat1-5000': { question: 'Question 1 for Cat 1 - 5000', answer: 'Answer 1 for Cat 1 - 5000' },
-  'cat2-5000': { question: 'Question 2 for Cat 2 - 5000', answer: 'Answer 2 for Cat 2 - 5000' },
-  'cat3-5000': { question: 'Question 3 for Cat 3 - 5000', answer: 'Answer 3 for Cat 3 - 5000' },
-  'cat4-5000': { question: 'Question 3 for Cat 3 - 5000', answer: 'Answer 3 for Cat 3 - 5000' },
-  'cat5-5000': { question: 'Question 3 for Cat 3 - 5000', answer: 'Answer 3 for Cat 3 - 5000' }
+  'cat1-5000': { question: 'Yohan, Meron, Penryn, Sandy, Bridge, Haswell', answer: 'Answer: Intel Processors' },
 
-  // Add all other questions here...
+  // TLEs
+  'cat2-1000': { question: 'The words abbreviated by ".csv"', answer: 'Answer: Comma Separated Variables/Values' },
+  'cat2-2000': { question: 'The words abbreviated by ".pdf"', answer: 'Answer: Portable Document Format' },
+  'cat2-3000': { question: 'The words abbreviated by ".mp3"', answer: 'Answer: Joint Photographic Experts Group' },
+  'cat2-4000': { question: 'The words abbreviated by ".tar"', answer: 'Answer: Tape Archive' },
+  'cat2-5000': { question: 'The words abbreviated by ".html"', answer: 'Answer: HyperText Markup Language' },
+
+  // Hard CIDR              
+  'cat3-1000': { question: 'A /24 network with a host of 192.168.0.100 has this subnet mask', answer: 'Answer: 255.255.255.0' },
+  'cat3-2000': { question: 'A /26 network has how many addresses (estimate)', answer: 'Answer: 65,534' },
+  'cat3-3000': { question: 'The C in CIDR stands for this, also a not great person', answer: 'Answer: Classless' },
+  'cat3-4000': { question: 'A /29 network has how many usable addresses', answer: 'Answer: 6' },
+  'cat3-5000': { question: 'A /31 subnet is typically used for this', answer: 'Answer: Point-to-Point links' },
+
+  // Randomness  
+  'cat4-1000': { question: 'The default port that DNS uses', answer: 'Answer: 53' },
+  'cat4-2000': { question: 'The numerical code for a successful HTTP request?', answer: 'Answer: 200' },
+  'cat4-3000': { question: 'The default port that telnet uses', answer: 'Answer: 23' },
+  'cat4-4000': { question: 'Pi with at least 8 digits to the right of the decimal point', answer: 'Answer: 3.14159265' },
+  'cat4-5000': { question: 'What is the answer to the ultimate question of life, the universe and everything', answer: 'Answer: 42' },
+
+  // Security Acronyms
+  'cat5-1000': { question: 'SIEM', answer: 'Answer: Security Information and Event Management' },
+  'cat5-2000': { question: 'CISO', answer: 'Answer: Chief Information Security Officer' },
+  'cat5-3000': { question: 'ISACA', answer: 'Answer: Information Systems Audit and Control Association' },
+  'cat5-4000': { question: 'SANS', answer: 'Answer: System Administration, Networking and Security Institute' },
+  'cat5-5000': { question: 'RBAC', answer: 'Answer: Role-Based Access Control' }
 };
 
 let currentBuzzer = null;
